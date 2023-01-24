94257a13a6ced4ca0dd7b1bc9cf325df0637bd60
RalfKonrad <github@eckel-consulting.de>
2023-01-24 22:47:49 +0100

    Trying to reproduce the bug.

--- compilerbug.cpp
+++ compilerbug.cpp
@@ -1,38 +1,51 @@
 //
 // Created by ralf.eckel on 24.01.2023.
 //
+
+#include <exception>
 #include <iostream>
 #include <memory>
 
 class Abstract {
   public:
-    Abstract(){};
-    Abstract(const Abstract &other) : mA(other.mA){};
-
+    explicit Abstract() : a_(0) {}
+    Abstract(const Abstract&) {}
+    Abstract& operator=(const Abstract&) { return *this; }
     virtual ~Abstract() = default;
 
-    void setA(double inA) { mA = inA; };
+    void increaseA() { a_++; }
+    int a() { return a_; }
 
-    double getA() { return mA; };
-
-    virtual void isAbstract() = 0;
-
-  protected:
-    double mA;
+  private:
+    int a_;
 };
 
 class Parent : public virtual Abstract {
   public:
-    Parent(){};
-    Parent(const Parent &other) : Abstract(other){};
+    explicit Parent(int p) : p_(p) {}
+
+    int p() { return p_; }
+
+  private:
+    int p_;
 };
 
-class Child : public virtual Parent {
+class Child : public Parent {
   public:
-    Child(){};
-    Child(const Child &other) : Parent(other){};
+    explicit Child(int p) : Parent(p) {}
+    explicit Child(const std::shared_ptr<Parent>& parent) : Child(makeChild(parent)) {}
 
-    void isAbstract(){};
+  private:
+    static Child makeChild(const std::shared_ptr<Parent>& parent) {
+        auto child = std::dynamic_pointer_cast<Child>(parent);
+        if (child == nullptr) {
+            throw std::exception("No child given");
+        }
+        return Child(child->p());
+    }
 };
 
-int main() { return 0; }
+int main() {
+    auto child = std::make_shared<Child>(42);
+    auto copiedChild = Child(child);
+}
